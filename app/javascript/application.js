0bcd8adc316e9189c07bc5d3ba35c6bd77d37509
alexis stephan <alexis-stephan@hotmail.fr>
2024-02-26 22:04:13 +0100

    setup
    Signed-off-by: alexis stephan <alexis-stephan@hotmail.fr>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+
+// adf1f2d7
+// 48727053
+// 8691812a
+// 620c6871
+// d420fcf5
+// 6ddb271d
+// f6da2807
+
+
+// http://www.omdbapi.com/?s=batman&apikey=adf1f2d7
